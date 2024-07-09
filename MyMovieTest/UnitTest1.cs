d6fcc81bd1acef9d3c781ba1738c4ec602aeec9d
prakash-innopad <prakashkadam@innopadsolutions.com>
2024-07-09 17:45:19 +0530

    Update UnitTest

--- MyMovieTest/UnitTest1.cs
+++ MyMovieTest/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace MyMovieTest
         [Fact]
         public void Test1()
             {
-             Assert.True(1 == 0);
+             Assert.True(1 == 1);
             }
         }
     }
