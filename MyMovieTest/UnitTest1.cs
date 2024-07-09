aa71e39a6021ae069834aa79591cdfd1b80d249e
prakash-innopad <prakashkadam@innopadsolutions.com>
2024-07-09 17:43:23 +0530

    Check for Fail test

--- MyMovieTest/UnitTest1.cs
+++ MyMovieTest/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace MyMovieTest
         [Fact]
         public void Test1()
             {
-             Assert.True(1 == 1);
+             Assert.True(1 == 0);
             }
         }
     }
